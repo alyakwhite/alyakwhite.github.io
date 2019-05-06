09f98bb52e201cae6de21b01dc867d6034c95905
Kayla White <kaykayywhite82@gmail.com>
2019-05-06 23:01:51 +0000

    Added Background to Game

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -36,7 +36,7 @@ var background = function(window) {
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
             var backgroundFill = draw.rect(canvasWidth, canvasHeight, 'black');
-            background.addChild(backgroundFill);
+            //background.addChild(backgroundFill);
 
             // TODO: 3 - Add a moon and starfield
             var circle;
